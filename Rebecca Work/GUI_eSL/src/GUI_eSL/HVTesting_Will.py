e775d8a452bcf3eac4765b6f87f5c9d407237809
Will <willgraham345@gmail.com>
2022-09-06 15:01:14 -0600

    Added how to connect with HV Supply SOP
    Really important, wrote down how to set up the HV supply

--- Rebecca Work/GUI_eSL/src/GUI_eSL/HVTesting_Will.py
+++ Rebecca Work/GUI_eSL/src/GUI_eSL/HVTesting_Will.py
@@ -1,15 +1,17 @@
 from SpellmanHvSupply import  SpellmanHVSupply
-
+import time
 HVSupply1 = SpellmanHVSupply()
 if __name__ == '__main__':
-    HVSupply1.openComm()
+    a = HVSupply1.openComm()
     print("opened  communication")
-
-    HVSupply1.closeComm()
-    print("closed communication")
-
-    HVSupply1.HVOutput(True)
+    HVSupply1.HVOutput(1)
     print("Turned HV on")
+    time.sleep(1)
 
     HVSupply1.HVOutput(False)
     print("Turned HV off")
+
+
+    # c = HVSupply1.systemStatus()
+    b = HVSupply1.closeComm()
+    print("closed communication")
