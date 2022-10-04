96513bc4d8389855ec42c6afe3e05d76acaac5c5
Will Desktop - Home <willgraham345@gmail.com>
2022-10-03 18:03:10 -0700

    Reorganized, started HV tuning
    Needs more work with capacitor tuning. Talk w/Dr Drew about capacitor tuning.

---
+++ Circuit Analysis/HV_Circuit_Calculations.m
@@ -0,0 +1,12 @@
+%%
+clear; close all; clc; format compact;
+
+V_in = 4.1*1000; %voltage in
+I_out = .002; %A
+R = V_in/I_out;
+
+% Define RC System
+
+ts_des = 
+C = 10; %nF
+ts_r = 
--- Rebecca Work/GUI_eSL/setup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# Third Party
-import setuptools
-
-setuptools.setup()
